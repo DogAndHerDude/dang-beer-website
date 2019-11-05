9c5462207c40bafb23589f6d924f53ae92505a70
Dominykas Bartkus <dominykasbart@gmail.com>
2019-11-05 10:36:06 +0200

    Added a temp favicon. Fixed mobile styles for the about page. Fixed padding on the ocntact and about pages.

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,17 +41,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `dangbeer`,
+        short_name: `dngbeer`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#1a202c`,
+        theme_color: `#f6e05e`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/dngbeer-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }

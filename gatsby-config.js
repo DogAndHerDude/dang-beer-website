03c736b11f28f9ab42845028afdaf761e198c8f7
Dominykas Bartkus <dominykasbart@gmail.com>
2019-11-05 12:34:58 +0200

    Added tracking analytics.

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,5 +53,9 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      trackingId: `UA-151544311-1`,
+    },
   ],
 }
